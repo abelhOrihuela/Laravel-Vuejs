a9eea3826e9e3a07086a3c33bed436330c0bc247
Abelh Orihuela <abelorihuelamendoza@hotmail.com>
2016-10-27 21:38:00 -0500

    Test router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,17 +1,36 @@
-var Vue = require('vue/dist/vue.js')
+var Vue = require('vue/dist/vue.js');
+
+var VueResource = require('vue-resource/dist/vue-resource.js');
+var VueRouter = require('vue-router/dist/vue-router.js');
 
 
-new Vue({
-  el: '#app',
-  data: {
-    input: '# hello'
-  },
-  computed: {
-    compiledMarkdown: function () {
-      return marked(this.input, { sanitize: true })
-    }
-  },
-  methods: {
-    
-  }
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+
+const router = new VueRouter({
+  routes // short for routes: routes
 })
+
+
+const app = new Vue({
+  router,
+  data:{
+  	message: "Keytalent"
+  }
+}).$mount('#app')
+
+
+
+
+
+
--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -6,14 +6,26 @@
     <script src="/vendor/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="/vendor/css/bootstrap.min.css" media="screen" title="no title">
 </head>
-<body id="app">
+<body >
 
-{{ 2 + 2 }}
-
-<input type="text" name="" class="form-control">
-
-
-
+<div id="app">
+     <h1>Hello App!</h1>
+  <p>
+    <!-- use router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- <router-link> will be rendered as an `<a>` tag by default -->
+    <router-link to="/foo">Go to Foo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+  </p>
+</div>
  <script src="{{ elixir('js/app.js') }}"></script>
+
+
+
+
+
+
+
+
 </body>
 </html>
